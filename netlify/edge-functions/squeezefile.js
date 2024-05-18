cc664c17a63768d95237aee6bb465740031f52ae
JensMatthiaschk <jens.matthiaschk@gmail.com>
2024-05-18 21:00:22 +0200

    moved email function

--- netlify/edge-functions/squeezefile.js
+++ netlify/edge-functions/squeezefile.js
@@ -1,6 +1,6 @@
 import { Client } from 'https://esm.sh/@octoai/client';
 import { encodingForModel } from 'https://esm.sh/js-tiktoken';
-import sendMail from './sendMail.js';
+import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
 
 
 if (!Netlify.env.get("OCTOAI_TOKEN")) {
@@ -8,6 +8,56 @@ if (!Netlify.env.get("OCTOAI_TOKEN")) {
 }
 
 const client = new Client(Netlify.env.get("OCTOAI_TOKEN"));
+
+
+const emailClient = new SMTPClient({
+    connection: {
+        hostname: Netlify.env.get("SMTP_HOST"),
+        port: 465,
+        tls: true,
+        auth: {
+            username: Netlify.env.get("SMTP_USER"),
+            password: Netlify.env.get("SMTP_PASS")
+        },
+    },
+});
+
+
+async function sendMail(emailContent) {
+
+    const content = `
+    <h3>Datei wurde erfolgreich verarbeitet:</h3>\n
+    <br>\n
+    <h4><u>Dateiname:</u></h4> <p>${emailContent.filename}</p>\n
+    <br>\n
+    <h4><u>genutzte Token bei Abfrage:</u></h4> <p>${emailContent.usedToken}</p>\n
+    <br>\n
+    <h4><u>gesamte Länge der Abfrage:</u></h4> <p>${emailContent.textLength}</p>\n
+    <br>\n
+    <h4><u>Anzahl der Abfragen/Chunks:</u></h4> <p>${emailContent.faktor}</p>\n
+    <br>\n
+    <h4><u>Modell:</u></h4> <p>${emailContent.model}</p>\n
+    `;
+
+    try {
+        const res = await emailClient.send({
+            from: "squeeze@noreply.com",
+            to: Netlify.env.get("RECEIVING_EMAIL_ADDRESS"),
+            subject: "Squeeze Log " + new Date().toISOString().slice(0, 19).replace("T", " "),
+            //content: content,
+            html: content,
+        });
+
+        await emailClient.close();
+
+        if (res.status === 250) {
+            return res.status(200).send({ message: "Email sent" });
+        } else {
+            return res.status(400).send({ message: "Email not sent" });
+        }
+    } catch (error) { return error }
+}
+
 
 
 export default async function (req) {
